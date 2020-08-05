6616870d63c0caa75455834e9c42106238f6452e
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-08-05 17:52:45 -0300

    :lipstick: Implements page header

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,24 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import backIcon from "../../assets/images/icons/back.svg";
-import logoImg from "../../assets/images/logo.svg";
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
 
 function TeacherForm() {
   return (
-    <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar" />
-          </Link>
-          <img src={logoImg} alt="Proffy logo" />
-        </div>
-
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis</strong>
-        </div>
-      </header>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
     </div>
   );
 }
