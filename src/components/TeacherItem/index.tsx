1c76565f2fae738ddbdfe293c72922fda58a16ff
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-08-09 19:43:57 -0300

    :sparkles: Create connection

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
 export interface Teacher {
   id: number;
@@ -17,6 +18,16 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  async function createNewConnection() {
+    try {
+      await api.post("/connections", {
+        user_id: teacher.id,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -34,7 +45,12 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
         </a>
